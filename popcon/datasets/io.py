e7b0e6725e76bba48bee5c2890072d8fabd45f75
v715 <vgopala4@jhu.edu>
2019-12-27 12:17:31 +0530

    Add simpler functionality

--- popcon/datasets/io.py
+++ popcon/datasets/io.py
@@ -1,8 +1,12 @@
+from itertools import takewhile
+from os import listdir
 from os.path import abspath, dirname, join
 
+import networkx as nx
 import numpy as np
 import pandas as pd
 from sklearn.utils import Bunch
+from tqdm import tqdm
 
 from ..utils import Multigraph
 
@@ -13,12 +17,75 @@ def _get_datapath(path):
     return join(root, "data", path)
 
 
-def _load_dataset(data_path):
-    """Generage `Multigraph` object for a given dataset."""
+def _parse_participants(data_path):
 
-    # Read the participant descriptor and graph tensor
-    participants = pd.read_csv(join(data_path, "participants.csv"))
-    graphs = np.load(join(data_path, "graphs.npy"))
+    participants_path = join(data_path, "participants.csv")
+
+    # Get the dictionary of parameters
+    param_dict = {}
+    with open(participants_path, "r") as fobj:
+        header_iter = takewhile(lambda s: s.startswith("#"), fobj)
+        for item in header_iter:
+            parameter, value = item[1:].split(":")
+            parameter = parameter.strip()
+            value = value.strip()
+            param_dict[parameter] = value
+
+    # Read the participants data
+    participants = pd.read_csv(participants_path, comment="#")
+
+    return participants, param_dict
+
+
+def _load_edgelists(data_path, participants, sample_size, n_vertices, extension):
+
+    # Make an empty tensor to hold the graphs
+    graphs = np.zeros(shape=(sample_size, n_vertices, n_vertices))
+
+    # Iterate over graphs and store them in `participants`
+    for fl in tqdm(listdir(join(data_path))):
+
+        if not fl.endswith(extension):
+            continue
+
+        subid = fl.split("_")[0]
+        idx = participants.index[participants["participant_id"] == subid]
+
+        with open(join(data_path, fl), "rb") as edgelist:
+            G = nx.read_edgelist(edgelist)
+            adj = nx.to_numpy_array(G, nodelist=sorted(G.nodes), dtype=np.float)
+            graphs[idx[0]] = adj
+            participants.at[idx, "slice"] = idx[0]
+
+    return graphs
+
+
+def _load_dataset(data_path, extension):
+    """
+    Generage `Multigraph` object for a given dataset.
+
+    Parameters
+    ----------
+    data_path : Path
+        Path to BIDS-formatted dataset
+    extension : string
+        File extension for all edgelists in the dataset
+
+    Returns
+    -------
+    Multigraph : Multigraph
+        Multigraph object for the dataset
+    """
+
+    # Read the participant descriptor
+    participants, param_dict = _parse_participants("data_path")
+
+    # Read the graphs
+    sample_size = int(param_dict["sample_size"])
+    n_vertices = int(param_dict["n_vertices"])
+    graphs = _load_edgelists(
+        data_path, participants, sample_size, n_vertices, extension
+    )
 
     return Multigraph(participants, graphs)
 
