dc99db8e454b8becb8a2615d4c950378d4afbe31
v715 <vgopala4@jhu.edu>
2019-12-27 08:44:52 +0530

    Add setup file

---
+++ setup.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from sys import platform
+
+PACKAGE_NAME = "popcorns"
+DESCRIPTION = "Population-level connectome analysis in Python!"
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+AUTHOR = ("Vivek Gopalakrishnan",)
+AUTHOR_EMAIL = "vgopala4@jhu.edu"
+URL = "https://github.com/v715/popcorns"
+MINIMUM_PYTHON_VERSION = 3, 5
+REQUIRED_PACKAGES = [
+    "Click>=7.0",
+    "graspy>=0.1",
+    "mgc>=0.0.1",
+    "pandas>=0.25.0",
+    "scikit-bio>=0.5.5",
+    "statsmodels>=0.10.0",
+    "tqdm>=4.41.0",
+]
+
+# Find popcorns version
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+for line in open(os.path.join(PROJECT_PATH, "popcorns", "__init__.py")):
+    if line.startswith("__version__ = "):
+        VERSION = line.strip().split()[2][1:-1]
+
+
+def check_python_version():
+    """Exit when the Python version is too low."""
+    if sys.version_info < MINIMUM_PYTHON_VERSION:
+        sys.exit("Python {}.{}+ is required.".format(*MINIMUM_PYTHON_VERSION))
+
+
+check_python_version()
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    install_requires=REQUIRED_PACKAGES,
+    url=URL,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite="tests",
+)
