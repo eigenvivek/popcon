6a2ddc5faf1074d586c276b443d8a52de9620a32
v715 <vgopala4@jhu.edu>
2019-12-31 11:20:25 +0530

    Update multigraph querying mechanism

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,4 +3,10 @@ from popcon.datasets import *
 
 
 def test_mice():
-    multigraph = load_mice()
+
+    # Load the mouse dataset
+    mice = load_mice()
+
+    # Subset the BTBR genotype
+    _, btbr = mice.multigraph.query("genotype == 'BTBR'")
+    assert len(btbr) == 8
