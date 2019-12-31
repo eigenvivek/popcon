7ea88d187be91ec255f9053a5693e33162700154
v715 <vgopala4@jhu.edu>
2019-12-31 11:23:20 +0530

    Exclude tests

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     test_suite="tests",
 )
