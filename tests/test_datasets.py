108aa7cdaa30115551e8c7a040fa9542b8e46603
v715 <vgopala4@jhu.edu>
2020-01-02 15:11:20 +0530

    Flesh out test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,6 +1,16 @@
 import pytest
-from popcon.datasets import *
+
+from popcon.datasets import load_mice
 
 
 def test_mice():
-    multigraph = load_mice()
+
+    mice = load_mice()
+
+    _, btbr = mice.multigraph.query("genotype == 'BTBR'")
+    _, c57 = mice.multigraph.query("genotype == 'C57'")
+    _, cast = mice.multigraph.query("genotype == 'CAST'")
+    _, db2 = mice.multigraph.query("genotype == 'DB2'")
+
+    assert mice.multigraph.df.shape == (32, 3)
+    assert mice.blocks.shape == (14, 4)
