9ca45adc4cc8ae84ca65cacecf8bc7e914770f63
v715 <vgopala4@jhu.edu>
2020-06-16 12:13:20 -0400

    Add helper function for metrics

--- popcon/utils/__init__.py
+++ popcon/utils/__init__.py
@@ -1,4 +1,4 @@
 from .checks import check_input_graphs, check_multigraphs
 from .fmri import timeseries_to_connectome
-from .load import load_edgelists, parse_participants
+from .load import load_edgelists, load_metrics, parse_participants
 from .multigraph import Multigraph
--- popcon/utils/load.py
+++ popcon/utils/load.py
@@ -28,13 +28,13 @@ def parse_participants(data_path):
     return participants, param_dict
 
 
-def load_edgelists(data_path, participants, sample_size, n_vertices, extension):
+def load_edgelists(graph_path, participants, sample_size, n_vertices, extension):
 
     # Make an empty tensor to hold the graphs
     graphs = np.zeros(shape=(sample_size, n_vertices, n_vertices))
 
     # Iterate over graphs and store them in `participants`
-    for fl in tqdm(listdir(join(data_path))):
+    for fl in tqdm(listdir(graph_path), "Loading graphs:"):
 
         if not fl.endswith(extension):
             continue
@@ -42,10 +42,29 @@ def load_edgelists(data_path, participants, sample_size, n_vertices, extension):
         subid = fl.split("_")[0]
         idx = participants.index[participants["participant_id"] == subid]
 
-        with open(join(data_path, fl), "rb") as edgelist:
+        with open(join(graph_path, fl), "rb") as edgelist:
             G = nx.read_edgelist(edgelist)
             adj = nx.to_numpy_array(G, nodelist=sorted(G.nodes), dtype=np.float)
             graphs[idx[0]] = adj
             participants.at[idx, "slice"] = idx[0]
 
     return graphs
+
+
+def load_metrics(metrics_path, extension=".csv"):
+
+    holder = []
+
+    for fl in tqdm(listdir(metrics_path), "Loading metrics"):
+
+        if not fl.endswith(extension):
+            continue
+
+        subid = fl.split(".")[0]
+        df = pd.read_csv(join(metrics_path, fl), skiprows=2)
+        df["participant_id"] = subid
+        holder.append(df)
+
+    metrics = pd.concat(holder, axis=0)
+    metrics.reset_index()
+    return metrics
